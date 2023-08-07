f4761f1b1fe7afb7ab3c071f946baed5cff8d8be
Kamil Janeček <j@necek.com>
2023-08-07 15:46:16 +0200

    Support toggle switch

--- plus1-hue-switch.js
+++ plus1-hue-switch.js
@@ -1,5 +1,6 @@
 let CONFIG = {
     INPUT_ID: '0',
+    SWITCH_TYPE: 'TOGGLE', // {TOGGLE, EDGE}
     HUE_HUB_IP: '192.168.1.1',
     HUE_LIGHT_ID: '1',
     HUE_KEY: '<api>'
@@ -12,36 +13,44 @@ let isLightOn = function(response) {
     }
 };
 
-let switchLightState = function(currentState) {
-    print('switching to: ' + JSON.stringify(!currentState))
+let invertLightState = function(currentState) {
+    switchLightToState(!currentState)
+};
+
+let switchLightToState = function(wantedState) {
+    print('switching to: ' + JSON.stringify(wantedState))
     Shelly.call(
         "HTTP.Request", {
             method: 'PUT',
             url: "http://" + CONFIG.HUE_HUB_IP + "/api/" + CONFIG.HUE_KEY + "/lights/" + CONFIG.HUE_LIGHT_ID + "/state",
-            body: currentState ? '{"on":false}' : '{"on": true}'
+            body: wantedState ? '{"on":true}' : '{"on": false}'
         },
     );
 };
 
 let processHueResponse = function(result, error_code, error) {
-    switchLightState(
+    invertLightState(
         isLightOn(result)
     );
 };
 
-let switchLight = function() {
+let switchLight = function(switchState) {
+  if (CONFIG.SWITCH_TYPE === 'EDGE') {
     Shelly.call(
         "HTTP.GET", {
             url: "http://" + CONFIG.HUE_HUB_IP + "/api/" + CONFIG.HUE_KEY + "/lights/" + CONFIG.HUE_LIGHT_ID
         },
         processHueResponse
     );
+  } else if (CONFIG.SWITCH_TYPE === 'TOGGLE') {
+      switchLightToState(switchState)
+  }
 };
 
 Shelly.addEventHandler(function(event) {
     if (event.component === "input:" + CONFIG.INPUT_ID) {
         if (event.info.event === 'toggle') {
-            switchLight();
+            switchLight(event.info.state);
         }
     }
 }, null);
