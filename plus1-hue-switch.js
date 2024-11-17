7f13b0bb580aa178742ea8de66b8575f1a402b47
Kamil Janeček <j@necek.com>
2024-11-17 21:08:23 +0100

    Add HUE_LINKED_LIGHT_IDS

--- plus1-hue-switch.js
+++ plus1-hue-switch.js
@@ -3,12 +3,23 @@ let CONFIG = {
     SWITCH_TYPE: 'TOGGLE', // {TOGGLE, EDGE}
     HUE_HUB_IP: '192.168.1.1',
     HUE_LIGHT_ID: '1',
+    HUE_LINKED_LIGHT_IDS: ['2'],
     HUE_KEY: '<api>'
 };
 
+let setLightStateTo = function(lightId, wantedState) {
+      Shelly.call(
+        "HTTP.Request", {
+            method: 'PUT',
+            url: "http://" + CONFIG.HUE_HUB_IP + "/api/" + CONFIG.HUE_KEY + "/lights/" + lightId + "/state",
+            body: wantedState ? '{"on":true}' : '{"on": false}'
+        },
+    );
+}
+
 let isLightOn = function(response) {
-    if (response.code === 200) {
-        let jsonBody = JSON.parse(response.body);
+    if (result.code === 200) {
+        let jsonBody = JSON.parse(result.body);
         return jsonBody.state.on;
     }
 };
@@ -19,13 +30,11 @@ let invertLightState = function(currentState) {
 
 let switchLightToState = function(wantedState) {
     print('switching to: ' + JSON.stringify(wantedState))
-    Shelly.call(
-        "HTTP.Request", {
-            method: 'PUT',
-            url: "http://" + CONFIG.HUE_HUB_IP + "/api/" + CONFIG.HUE_KEY + "/lights/" + CONFIG.HUE_LIGHT_ID + "/state",
-            body: wantedState ? '{"on":true}' : '{"on": false}'
-        },
-    );
+    
+    setLightStateTo(CONFIG.HUE_LIGHT_ID, wantedState);
+    CONFIG.HUE_LINKED_LIGHT_IDS.forEach(function(id) { 
+      setLightStateTo(id, wantedState); 
+    })
 };
 
 let processHueResponse = function(result, error_code, error) {
